981a387a8eb4d2a22014be7c339bb345a9ea8c19
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-20 16:19:03 +0100

    Complete third excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let string = phrase
+    let phraseArray = string.split(""); 
+    let reversePhraseArray = ""
+        for (let i = phraseArray.length - 1; i >= 0; i--)
+            {
+                reversePhraseArray += phraseArray[i]
+                
+            }
+        return reversePhraseArray;
+        
 };
-
 // Do not edit below this line
 module.exports = reverseString;

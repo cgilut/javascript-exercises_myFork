2cdbc6d53f875675dd3b2ede3917edab7ea20221
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-20 14:43:59 +0100

    Complete second excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+"use strict"
+const repeatString = function (phrase, numberOfRepetitions) {
 
-};
+  if (numberOfRepetitions < 0) 
+    return "ERROR";
+  let string = "";
+  for (let i = 0; i < numberOfRepetitions; i++) 
+    {
+      string += phrase
+    }
+  return string;
+  };
 
-// Do not edit below this line
 module.exports = repeatString;

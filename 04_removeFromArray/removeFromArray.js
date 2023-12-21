4f3e1a39a91f6b539ab5c27b54fe4fe9ca42f575
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-21 13:46:54 +0100

    Complete fourth excercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeArguments) {
+    const modifiedArray = array.filter(function (element) 
+    {
+        return !removeArguments.includes(element);
+    });
+    return modifiedArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+

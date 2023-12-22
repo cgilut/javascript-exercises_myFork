e6c46e490adb231bbef932198d3c1475a12f92d6
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-22 10:46:20 +0100

    Complete fifth excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    // create variable to hold the sum
+    let sum = 0
+    let minValue = Math.min(a, b)
+    let maxValue = Math.max(a, b)
+    // if a number is negative or a non-number return ERROR
+    if (
+        a < 0 ||
+        typeof a !== "number" ||
+        b < 0 ||
+        typeof b !== "number"
+        ) {
+            return "ERROR";
+        }
+    // find the smallest and start the loop from it
+    else {
+        for (let i = minValue; i <= maxValue; i++) {
+            sum = sum + i;         
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+

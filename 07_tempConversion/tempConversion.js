343e332b4cc61437bcb065dfd419913f862e5083
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-22 11:37:04 +0100

    Complete seventh excercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+      let tempCelsius = ((tempFahrenheit - 32) / 9 * 5).toFixed(1);
+      return +tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+      let tempFahrenheit = (tempCelsius / 5 * 9 + 32).toFixed(1);
+      return +tempFahrenheit;
 };
 
 // Do not edit below this line

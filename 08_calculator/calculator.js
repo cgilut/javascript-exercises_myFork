22a1b3685d7e93dfcad8e7dffcccc3423bf3ac0f
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-22 13:17:21 +0100

    Complete eighth excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let resultAdd = a + b
+  return +resultAdd;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  let resultSubtract = a - b
+  return +resultSubtract;
 };
 
-const sum = function() {
-	
+const sum = function(arraySum) {
+	let resultSum = 0
+  for (let i = 0; i < arraySum.length; i++) {
+    resultSum += arraySum[i]
+  }
+  return resultSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayMultiply) {
+  let resultMultiply = 1
+  for (let i = 0; i < arrayMultiply.length; i++) {
+    resultMultiply = resultMultiply * arrayMultiply[i]
+  }
+  return resultMultiply;
 };
 
-const power = function() {
-	
+const power = function(number, power) { 
+	let resultFunction = 1
+  for (let i = 1; i <= power; i++) {
+    resultFunction = resultFunction * number
+  }
+  return resultFunction;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a < 2) {
+    return 1;
+  }
+  else {
+    let resultFactorial = a
+    for (let i = a; i > 1; i--) {
+      resultFactorial = resultFactorial * (i - 1)
+    }
+    return resultFactorial;
+  }
 };
 
 // Do not edit below this line

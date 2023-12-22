77c6d3ffbee13c86a6947df391b1ffa55fa3c241
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-22 11:23:28 +0100

    Complete sixth excercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // if year is devided by 400 then its leap\
+    if (year % 400 === 0) {
+        return true;
+    }
+    if (
+        year % 4 === 0 &&
+        year % 100 !== 0
+    ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    // if year is devided by 4 AND NOT devided by 100 then its leap
 
 };
 

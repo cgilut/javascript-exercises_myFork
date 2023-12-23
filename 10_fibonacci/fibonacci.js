585f00a9f08ab9d802b7763a073d1304ffdf182f
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-23 16:33:26 +0100

    Complete tenth excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(specificMember) {
+    if (+specificMember < 0) 
+        {
+            return "OOPS";
+        }
+    if  (+specificMember == 0)
+        {
+            return 0;
+        }
+    if  (+specificMember < 3)
+        {
+            return 1;
+        }
+    else 
+        {
+            let fibonacciSequence = [1, 1]
+                for (let i = 2; i <= +specificMember; i++) {
+                    fibonacciSequence.push(fibonacciSequence[(i - 1)] + fibonacciSequence[(i - 2)])
+                }
+            return fibonacciSequence[+specificMember - 1];
+        }
 };
 
 // Do not edit below this line

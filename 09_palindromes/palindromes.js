12ae3158f6fdf2ad393dc5023a8c15ebb74b67f6
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-23 12:15:19 +0100

    Complete ninth excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (originalString) {
+    let reverseModifiedString = originalString.replace(/[\s\p{P}]/gu, "").split("").reverse().join("").toLowerCase()
+    let originalModifiedString = originalString.replace(/[\s\p{P}]/gu, "").toLowerCase()
+    if (reverseModifiedString == originalModifiedString) 
+        {
+           return true; 
+        }
+    else 
+        {
+            return false;
+        }
 };
 
+// palindromes("ZZZZ car, a man, a maracaz.")
 // Do not edit below this line
 module.exports = palindromes;

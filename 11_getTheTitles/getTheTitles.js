f63b5da3831919790f3ab2cc51615e8ff37a91eb
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-23 17:07:51 +0100

    Complete eleventh excercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,20 @@
-const getTheTitles = function() {
+const getTheTitles = function(getTitle) {
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ]
 
+    let arrayTitles = []
+    for (let i = 0; i < books.length; i++) {
+        arrayTitles = books.map(bookTitle => bookTitle.title)
+    }
+    return arrayTitles;
 };
 
 // Do not edit below this line

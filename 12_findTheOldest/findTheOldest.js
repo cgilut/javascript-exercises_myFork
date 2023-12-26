e1382b22bfe2f3b810f6c5534bc3c293a3a38169
cgilut <117078690+cgilut@users.noreply.github.com>
2023-12-26 20:46:53 +0100

    Complete twelfth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,49 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+  // let peopleAge = people
+  let currentYear = new Date().getFullYear()
+  let age
+  let maxAge = 0
+  let oldestPerson = ""
+
+  for (let i = 0; i < people.length; i++) {
+
+    let person = people[i]
+
+
+    if (!('yearOfDeath' in person) || person.yearOfDeath == ""){
+      age = currentYear - person.yearOfBirth
+    } else {
+      age = person.yearOfDeath - person.yearOfBirth
+    }
+    person.age = age
+
+    if (age > maxAge) {
+      maxAge = age
+      oldestPerson = person
+    }
+  }
+  return oldestPerson;
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+
+findTheOldest(people)
 
 // Do not edit below this line
 module.exports = findTheOldest;
